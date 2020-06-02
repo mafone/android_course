d1f72132ca9373885120e0c76f2fe14549d9a894
moises.ferreira <moises.ferreira@assec.local>
2020-06-02 18:07:29 +0100

    Solved exercises

---
+++ SolvedExercises/Lesson02-GitHub-Repo-Search/T02.04-Exercise-ConnectingToTheInternet/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.datafrominternet.utilities;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * These utilities will be used to communicate with the network.
+ */
+public class NetworkUtils {
+
+    final static String GITHUB_BASE_URL =
+            "https://api.github.com/search/repositories";
+
+    final static String PARAM_QUERY = "q";
+
+    /*
+     * The sort field. One of stars, forks, or updated.
+     * Default: results are sorted by best match if no field is specified.
+     */
+    final static String PARAM_SORT = "sort";
+    final static String sortBy = "stars";
+
+    /**
+     * Builds the URL used to query GitHub.
+     *
+     * @param githubSearchQuery The keyword that will be queried for.
+     * @return The URL to use to query the GitHub server.
+     */
+    public static URL buildUrl(String githubSearchQuery) {
+        Uri builtUri = Uri.parse(GITHUB_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_QUERY, githubSearchQuery)
+                .appendQueryParameter(PARAM_SORT, sortBy)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    //The following method will be used to  make an HTTP request. We'll pass our well formed URL to it
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection(); //Start http connection with our url
+        try {
+            InputStream in = urlConnection.getInputStream(); //Open the stream for http our request
+
+            Scanner scanner = new Scanner(in); //Using scanner to read each line on our stream (one of the existent objects for this purpose)
+            scanner.useDelimiter("\\A"); // \\A allows passing all the stream
+
+            boolean hasInput = scanner.hasNext(); //Evaluate if we have a valid input
+            if (hasInput) {
+                return scanner.next(); //Returning the result (next)
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect(); //After the try catch block where we got the result, we close the connection.
+        }
+    }
+}
